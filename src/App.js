6cbc6b30c79982a06aaaf2b1dd38e6859a55bab9
nvdai2401 <nvdai2401@gmail.com>
2020-02-14 17:03:32 +0700

    configured css, scss, file-loader loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import isArray from 'lodash/isArray'
+
+import './App.scss'
+
 class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			foo: [1, 2, 3],
+		}
+	}
+
+	checkFoo = () => {
+		return isArray(this.state.foo)
+	}
 	render() {
-		return <div>Home page</div>
+		return (
+			<div className='title'>
+				{this.checkFoo ? <p>Foo is an array</p> : <p>Foo is NOT an array</p>}
+			</div>
+		)
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,53 @@
-require('webpack')
-const settings = require('./webpack')
+const { resolve } = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const settings = {
+	entry: './src/index.js',
+	output: {
+		path: resolve(__dirname, 'build'),
+		publicPath: '/',
+		filename: 'bundle.js',
+	},
+	devServer: {
+		contentBase: './build',
+	},
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.(js|jsx)?$/,
+				exclude: /node_modules/,
+				use: ['babel-loader', 'eslint-loader'],
+			},
+			{
+				test: /\.(s[ac]ss|css)$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				loader: 'file-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: resolve('./index.html'),
+		}),
+		new LodashModuleReplacementPlugin(),
+	],
+	optimization: {
+		minimizer: [new UglifyJsPlugin()],
+		splitChunks: {},
+	},
+	externals: {
+		lodash: {
+			commonjs: 'lodash',
+			amd: 'lodash',
+			root: '_', // indicates global variable
+		},
+	},
+}
 
 module.exports = settings
